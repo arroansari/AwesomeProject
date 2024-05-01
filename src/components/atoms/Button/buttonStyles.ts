561aac4d53ee9785f25eda6c54f4bf7fc862ba7f
arroansari <armananso729@gmail.com>
2024-05-02 00:58:31 +0530

    add button style

--- src/components/atoms/Button/buttonStyles.ts
+++ src/components/atoms/Button/buttonStyles.ts
@@ -2,7 +2,11 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     mainContainer: {},
-    btnContainerStyle: {},
+    btnContainerStyle: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     buttonTextStyle: {}
 
 });
