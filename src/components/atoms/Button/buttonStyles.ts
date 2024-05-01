94641c2c03dc6007aa343352f31bb5dc7389a858
arroansari <armananso729@gmail.com>
2024-05-02 00:33:18 +0530

    add button text style from style sheet

--- src/components/atoms/Button/buttonStyles.ts
+++ src/components/atoms/Button/buttonStyles.ts
@@ -1,7 +1,8 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    mainContainer: {
-        flex:1
-    }
+    mainContainer: {},
+    btnContainerStyle: {},
+    buttonTextStyle: {}
+
 });
